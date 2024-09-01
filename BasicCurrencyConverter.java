910c91f77d0f1dce0c8f38e15eea20612ffd2a39
Dreamerc1 <chelsreid99@gmail.com>
2024-09-01 18:13:46 +0100

    Constructor to init exc rates

--- BasicCurrencyConverter.java
+++ BasicCurrencyConverter.java
@@ -3,6 +3,12 @@ import java.util.Map;
 
 public class BasicCurrencyConverter implements CurrencyConverter {
 
-    // A map to store exchange rates between currencies
+// A map to store exchange rates between currencies
     private Map<String, Double> exchangeRates;
-    
+
+// Constructor to initialize exchange rates
+    public BasicCurrencyConverter() {
+        exchangeRates = new HashMap<>();
+
+     
+    }
